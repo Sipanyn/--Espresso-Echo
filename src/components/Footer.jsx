27bcfb271833d37a0aa7dac4a968f6f4a8b06f83
sipan younan <sipannyounan@gmail.com>
2025-08-19 18:13:27 +0330

    change icon size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import { NavLink, Outlet } from "react-router";
 
 function Footer() {
   return (
-    <div className="flex flex-col justify-between h-dvh *:select-none">
+    <div className="flex flex-col justify-between h-dvh *:select-none ">
       <Outlet />
-      <div className="bg-white w-full *:text-gray-300 flex flex-row justify-between *:cursor-pointer border-t-1 border-gray-300 mt-2 pt-[19px] pb-[24px] pl-[35px] pr-[35px]">
+      <div className="bg-white w-full *:text-gray-300 flex flex-row justify-between *:cursor-pointer border-t-1 border-gray-300 mt-2 pb-2 pt-2 pl-[35px] pr-[35px] sm:pt-[19px] sm:pb-[24px]">
         <NavLink
           to="/"
-          className={`flex flex-col items-center ({ isActive }) => (isActive ? "active" : "")`}
+          className={`flex flex-col items-center text-sm sm:text-[16px]  ({ isActive }) => (isActive ? "active" : "")`}
         >
-          <svg className="size-8 ">
+          <svg className="size-6 sm:size-8 ">
             <use href="sprite.svg#home_icon" />
           </svg>
           Home
@@ -18,7 +18,7 @@ function Footer() {
           to="/cart"
           className={`flex flex-col items-center ({ isActive }) => (isActive ? "active" : "")`}
         >
-          <svg className="size-8 ">
+          <svg className="size-6 sm:size-8 ">
             <use href="sprite.svg#cart_icon" />
           </svg>
           Cart
@@ -27,7 +27,7 @@ function Footer() {
           to="/favorite"
           className={`flex flex-col items-center ({ isActive }) => (isActive ? "active" : "")`}
         >
-          <svg className="size-8 ">
+          <svg className="size-6 sm:size-8">
             <use href="sprite.svg#favorite_icon" />
           </svg>
           Favorite
@@ -36,7 +36,7 @@ function Footer() {
           to="/profile"
           className={`flex flex-col items-center ({ isActive }) => (isActive ? "active" : "")`}
         >
-          <svg className="size-8 ">
+          <svg className="size-6 sm:size-8">
             <use href="sprite.svg#profile_icon" />
           </svg>
           Profile
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 function HomePage() {
   return (
     <div className="flex flex-col p-2.5">
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between items-start">
         <div className="flex flex-col gap-2">
           <div className="flex flex-row items-center gap-2 text-burntOrange font-bold">
             <svg className="size-8 ">
@@ -15,7 +15,7 @@ function HomePage() {
         </div>
 
         <div className="flex flex-row items-center gap-2 text-gray-400 cursor-pointer hover:text-burntOrange">
-          <svg className="size-8 ">
+          <svg className="size-6 sm:size-8">
             <use href="/sprite.svg#login_icon" />
           </svg>
           <h2>Login</h2>
