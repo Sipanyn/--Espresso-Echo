871aa49bd69671139e0c2405b46def1b135187b4
sipan younan <sipannyounan@gmail.com>
2025-08-25 10:46:50 +0330

    vercel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,11 +16,9 @@ function HomePage() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (allProducts.length < 0) {
-      if (!isLoading && data && data.length > 0) {
-        dispatch(getAllProducts(data));
-        console.log(data);
-      }
+    if (!isLoading && data && data.length > 0 && allProducts.length === 0) {
+      dispatch(getAllProducts(data));
+      console.log("Dispatched products:", data);
     }
   }, [data, isLoading, dispatch, allProducts.length]);
 
