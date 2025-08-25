a2011c17412843c075d7d33ad9e573b674437cfa
sipan younan <sipannyounan@gmail.com>
2025-08-25 08:41:01 +0330

    vercel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -25,7 +25,7 @@ function HomePage() {
         // console.log(data);
       }
     }
-  }, [data, isLoading, allProducts.length, dispatch]);
+  }, [data, isLoading, allProducts?.length, dispatch]);
   return (
     <>
       {loginModel ? (
