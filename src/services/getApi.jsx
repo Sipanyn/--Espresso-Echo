ed05a42de7f6f8ab044bf60911350d306adc9fd3
sipan younan <sipannyounan@gmail.com>
2025-08-25 10:21:11 +0330

    vercel

--- src/services/getApi.jsx
+++ src/services/getApi.jsx
@@ -1,6 +1,16 @@
 async function getApi() {
-  let res = await fetch(`/api/products`);
-  let data = await res.json();
+  const baseUrl =
+    import.meta.env.MODE === "development"
+      ? "/api"
+      : "https://espresso-echo-mock-api.vercel.app";
+
+  const res = await fetch(`${baseUrl}/products`);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch: ${res.status}`);
+  }
+
+  const data = await res.json();
   return data;
 }
+
 export default getApi;
